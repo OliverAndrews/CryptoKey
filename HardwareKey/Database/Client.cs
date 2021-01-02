5f4435c31dbff618e18026898607af97efddc5d5
oliverandrews <jaysonphillipmitchell@gmail.com>
2021-01-02 15:17:20 -0500

    Added in serials database

--- HardwareKey/Database/Client.cs
+++ HardwareKey/Database/Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,29 @@ namespace HardwareKey.Database
 {
     class Client
     {
+        private string _file;
+        public Client(string file)
+        {
+            _file = file;
+        }
+
+        IEnumerable<string> RunQuery(string query)
+        {
+            List<string> results  = new List<string>();
+            using (var connection = new SqliteConnection($"Data Source ={_file}"))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        results.Add(reader.GetString(0));
+                    }
+                }
+            }
+
+            return results;
+        }
     }
 }
